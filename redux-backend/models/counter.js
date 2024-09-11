462fe3e036a157d4c5ab650169fef7a779c813e7
Sourabh Patil <95458021+sourabh0396@users.noreply.github.com>
2024-09-11 18:24:37 +0530

    Update counter.js

--- redux-backend/models/counter.js
+++ redux-backend/models/counter.js
@@ -1,16 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const counterSchema = new mongoose.Schema({
-//   // _id: String, // Identifier for the counter
-//   // sequence_value: Number
-  
-//   _id: { type: String, required: true }, // Sequence name
-//   sequence_value: { type: Number, default: 0 }, // Current sequence value
-// });
-
-// const Counter = mongoose.model('Counter', counterSchema);
-
-// module.exports = Counter;
 
 const mongoose = require("mongoose");
 
